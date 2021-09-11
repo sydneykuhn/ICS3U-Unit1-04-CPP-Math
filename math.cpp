5edc90c01c928d0653e69e8f6136f76b7ca64d06
Ubuntu <ubuntu@ip-172-31-90-143.ec2.internal>
2021-09-11 18:43:28 +0000

    added area and perimeter of a rectangle.cpp

--- math.cpp
+++ math.cpp
@@ -12,7 +12,7 @@ int main() {
     std::cout << "5cm x 3cm" << std::endl;
     std::cout << std::endl;
     std::cout << "Area is " << (5 * 3) << "cm²." << std::endl;
-    std::cout << "Perimeter is " << (2 * (5 + 3)) << "cm." << std::endl;
+    std::cout << "Perimeter is " << (2 * (5 + 3 )) << "cm." << std::endl;
     std::cout << std::endl;
     std::cout << "Done." << std::endl;
 }
